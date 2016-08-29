44f98d536378aa735027534e1f59e5c7383fce03
Max Silbiger <max.silbiger@gmail.com>
2016-08-29 17:06:55 -0400

    Complete user story: return json of searched images

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ var FLICKR_API_KEY = process.env.FLICKR_API_KEY;
 var FLICKR_SECRET_KEY = process.env.FLICKR_SECRET_KEY;
 var FLICKR_ENDPOINT = 'https://api.flickr.com/services/rest/';
 
-function flickrUrl(farmId, serverId, id, secret) {
-	return 'https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg';
+function flickrUrl(photo) {
+	return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+}
+
+function flickrThumbnailUrl(photo) {
+	return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_t.jpg';
 }
 
 app.use(nofavicon());
@@ -25,13 +29,24 @@ app.get('/api/imagesearch/:terms', function(req, res) {
 
 	var getUrl = FLICKR_ENDPOINT + '?method=flickr.photos.search&api_key=' + FLICKR_API_KEY + '&text=' + searchTerms + "&format=json" + "&nojsoncallback=1";
 
-	var apiCall = request(getUrl, function(err, res, body) {
+	var apiCall = request(getUrl, function(err, response, body) {
 		if (err) throw err;
-		var photos = JSON.parse(body);
-		var deepPhotos = util.inspect(photos, {showHidden: false, depth: null});
+		var photos = JSON.parse(body).photos.photo;
+		//var deepPhotos = util.inspect(photos, {showHidden: false, depth: null});
 		// ... for each photo in deepPhotos, get a bunch of its properties and assemble a response json.
 		// flickrUrl() is good for getting the urls.
-		console.log(deepPhotos);
+		console.log(photos);
+		//console.log(photos.length);
+		var resultArr = [];
+		photos.forEach(function(item) {
+			var photoObj = {
+				url: flickrUrl(item),
+				snippet: item.title,
+				thumbnail: flickrThumbnailUrl(item)
+			}
+			resultArr.push(photoObj);
+		});
+		res.send(resultArr);
 	});
 });
 
